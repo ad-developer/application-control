828623851087dd7e31a3a4fc08ba39e49efd4962
ad <ad@Mac.lan>
2025-05-24 16:12:31 -0700

    remove some files

--- src/ApplicationControl.Extensions/ApitClient/IApplicationControlServiceClient.cs
+++
@@ -1,7 +0,0 @@
-namespace ApplicationControl.Extensions.ApitClient;
-
-public interface IApplicationControlServiceClient
-{
-    //Task<CommandQueueItem?> GetNextCommandAsync(Guid applicationId, CancellationToken cancellationToken = default);
-    //Task SetCommandStatusAsync(Guid applicationId, Guid commandId, string setBy, CommandStatus commandStatus, string message, CancellationToken cancellationToken = default); 
-}
